b0d2383e0792a580f62537f9c9125f5dea270eba
lorena <36055216+LorenaMacias@users.noreply.github.com>
2020-01-09 17:34:20 -0800

    sql

---
+++ logs.sql
@@ -0,0 +1,37 @@
+CREATE TABLE users{
+id INTEGER PRIMARY KEY,
+username VARCHAR NOT NULL,
+pass VARCHAR NOT NULL,
+email VARCHAR NOT NULL,
+UNIQUE(username),
+UNIQUE(email)
+};
+
+CREATE TABLE foods{
+id INTEGER PRIMARY KEY,
+name VARCHAR NOT NULL,
+calories INTEGER,
+quantity INTEGER,
+fdate VARCHAR,
+consumer VARCHAR NOT NULL,
+FOREIGN KEY(consumer) REFERENCES users(username)
+};
+
+CREATE TABLE purchases{
+id INTEGER,
+title VARCHAR,
+cost FLOAT,
+quantity INTEGER,
+fdate VARCHAR,
+consumer VARCHAR NOT NULL,
+FOREIGN KEY(consumer) REFERENCES users(username)
+};
+
+CREATE TABLE exercises{
+id INTEGER PRIMARY KEY,
+workout VARCHAR,
+duration INTEGER,
+fdate VARCHAR,
+consumer VARCHAR NOT NULL,
+FOREIGN KEY(consumer) REFERENCES users(username)
+};
